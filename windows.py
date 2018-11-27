ca60f6fd4acb7f0a9f8f977bca387330a3677ba1
Clément Galland <clement.galland@epita.fr>
2018-11-27 17:44:26 +0100

    detect contour and display them

--- windows.py
+++ windows.py
@@ -2,8 +2,18 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+def detect_card(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
+    _, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_contours = sorted([ (cv2.contourArea(i), i) for i in contours ], key=lambda a:a[0], reverse=True)
+    for _, contour in sorted_contours:
+        cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
+
+
 while True:
     ret, frame = cap.read()
+    detect_card(frame)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
