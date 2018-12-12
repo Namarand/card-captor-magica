04c24778ad1e2f19dd7dc1f353feebbfc529d696
Clément Galland <clement.galland@epita.fr>
2018-12-12 12:43:15 +0100

    card_tools: move card function in its dedicated module

--- card_captor/card_tools/title.py
+++ card_captor/card_tools/title.py
@@ -6,25 +6,25 @@ from mtgsdk import Card
 import difflib
 import pytesseract
 
-def detect_characters(image_path):
-    return pytesseract.image_to_string(Image.open(image_path), lang='fra')
-
 def identify_card(image_path, database):
-    ocr_result = detect_characters(image_path)
+    ocr_result = _detect_characters(image_path)
     closest = database.find_closest(ocr_result)
     return closest
 
-def fetch_extension(extensions_id):
+def _detect_characters(image_path):
+    return pytesseract.image_to_string(Image.open(image_path), lang='fra')
+
+def _fetch_extension(extensions_id):
     return Card.where(set=extensions_id).all()
 
-def create_card_database(extensions_id, languages):
+def _create_card_database(extensions_id, languages):
     database = []
     for extension in extensions_id:
-        cards = fetch_extension(extension)
+        cards = _fetch_extension(extension)
         database += get_cards_name(cards, languages)
     return database
 
-def get_cards_name(cards, languages):
+def _get_cards_name(cards, languages):
     results = []
     for card in cards:
         if "English" in languages:
