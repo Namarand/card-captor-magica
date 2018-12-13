57227a33c10ad49ad2d389a087045d3f25d236cb
Clément Galland <clement.galland@epita.fr>
2018-12-13 12:37:36 +0100

    gui: home: camera video is now displayed on the home page

--- card_captor/gui/home.py
+++ card_captor/gui/home.py
@@ -1,13 +1,55 @@
+from tempfile import mkdtemp
+import cv2
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QVBoxLayout
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
 
+from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal, Qt
+from PyQt5.QtGui import QIcon, QImage, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QHBoxLayout, QLabel
+
+from output_handler import Output
+from trie import Trie
+from loop import process_frame
 
 class Home(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
-        self.layout = QVBoxLayout(self)
-        self.pushButton1 = QPushButton("Home")
-        self.layout.addWidget(self.pushButton1)
+        self._create_layout()
+
+    def _create_layout(self):
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(self._create_camera())
+        self.layout.addWidget(self._create_cards_list())
         self.setLayout(self.layout)
+
+    def _create_camera(self):
+        self.label = QLabel(self)
+        self.label.move(280, 120)
+        self.label.resize(640, 480)
+        th = CameraThread(self)
+        th.changePixmap.connect(self.setImage)
+        th.start()
+        return self.label
+
+    def _create_cards_list(self):
+        return QPushButton()
+
+    @pyqtSlot(QImage)
+    def setImage(self, image):
+        self.label.setPixmap(QPixmap.fromImage(image))
+
+class CameraThread(QThread):
+    changePixmap = pyqtSignal(QImage)
+
+    def run(self):
+        cap = cv2.VideoCapture(0)
+        database = Trie()
+        folder = mkdtemp()
+        output = Output()
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                frame = process_frame(frame, database, folder, output)
+                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
